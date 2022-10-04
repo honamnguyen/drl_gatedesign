71e0abcc361d2288b2462864cdfcd21706fcbeb9
Ho Nam Nguyen <honamnguyen95@gmail.com>
2022-10-03 23:45:45 -0700

    change default evaluationinterval to 500

--- drl/drl/scripts/rl_inference.py
+++ drl/drl/scripts/rl_inference.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     ### ----- PARSING ARGUMENTS ----- ###   
     parser = argparse.ArgumentParser()
     parser.add_argument('-run',default='0000',help='Name fragments of run. Default: 0000.')
+    parser.add_argument('-chpt',default='0000',help='Checkpoint. Default: 0000.')
     args = parser.parse_args()
 
     ### ----- LOAD CONFIG + UPDATE----- ###
@@ -25,23 +26,36 @@ if __name__ == '__main__':
     config = pickle.load(open(config_file, "rb"))
     config['num_workers'] = 0
     config['logger_config'] = {'type': 'ray.tune.logger.NoopLogger'}
-    # for key in config:
-    #     print(key,config[key])
+    for key in config:
+        print(key,config[key])
     agent = DDPG(config=config)
     
     # INFERENCE   
     run = config_file.replace('/params.pkl','')
+    config['env_config']['step_params']['reward_scheme'] = 'local-fidelity-difference-nli'
+    config['env_config']['step_params']['reward_type'] =  'worst'
     env = transmon_env_creator(config['env_config'])
 
     for checkpoint in glob.glob(f'{run}/checkpoint*'):
+        if args.chpt not in checkpoint:
+            continue
         agent.restore(checkpoint)        
-        episode_reward = 0
         done = False
         obs = env.reset()
-        actions = []
+        data = {
+            'channels': ['u01','d1'],
+            'pulse': [],
+            'avg_fids': [env.avg_fid],
+            'worst_fids': [env.fid],
+        }
         while not done:
             action = agent.compute_single_action(obs)
-            obs, reward, done, info = env.step(action)
-            episode_reward += reward
-            actions.append(action)
-        print(f'\n{checkpoint.split("/")[-1]}: {episode_reward}')
+            obs, reward, done, _ = env.step(action)
+            data['pulse'].append(env.prev_action)
+            data['avg_fids'].append(env.avg_fid)
+            data['worst_fids'].append(env.fid)     
+        data['pulse'] = np.array(data['pulse'])
+        data['avg_fids'] = np.array(data['avg_fids'])
+        data['worst_fids'] = np.array(data['worst_fids'])
+        pickle.dump(data, open(checkpoint.replace('checkpoint',f'RLPulse_{env.fid:.4f}')+'.pkl', 'wb') )
+        # np.save(checkpoint.replace('checkpoint',f'pulse_nli{episode_reward:.3f}')+'.npy', np.array(actions))
